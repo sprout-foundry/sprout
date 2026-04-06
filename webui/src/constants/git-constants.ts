91d70970e05e807042787b076d86f929e02f7ba9
Alan Price <alan@inicion.com>
2026-04-06 00:09:14 -0500

    Updates 5 files - Updates git sidebar to paginate file lists
    Add pagination to the git sidebar panel to improve performance when
    handling large file lists by initially displaying a subset of files and
    allowing incremental loading. Additionally, update CI workflows to
    enforce TypeScript strict mode auditing and standardize checkbox
    formatting in the TODO file.

---
+++ webui/src/constants/git-constants.ts
@@ -0,0 +1,8 @@
+/** Maximum number of files to render per section before showing "Show more" */
+export const MAX_FILES_PER_SECTION = 500;
+
+/** Initial number of files to render per section */
+export const MAX_FILES_INITIAL = 100;
+
+/** Number of files to load when user clicks "Show more" */
+export const LOAD_MORE_INCREMENT = 100;
